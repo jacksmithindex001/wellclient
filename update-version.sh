a714c0f97c2d9f7bd850f218c9e4c37945af08da
wangdd <wangdd@welljoint.com>
2017-12-28 10:53:56 +0800

    删除无用文件

--- update-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# config the version of wellclient
-UPDATE_VERSION=v2.8.20.RELEASE
-
-git fetch origin ${UPDATE_VERSION}:${UPDATE_VERSION}
-git ci -am "update the wellclient version"
-git checkout ${UPDATE_VERSION}
