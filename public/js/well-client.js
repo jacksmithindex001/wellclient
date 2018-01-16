fc73d2738b6892e5477543f56788195a74b9d13d
wangdd <wangdd@welljoint.com>
2018-01-16 12:05:00 +0800

    update version

--- public/js/well-client.js
+++ public/js/well-client.js
@@ -8,7 +8,7 @@ window.wellClient = (function ($) {
   }
 
   var Config = {
-    version: '4.1.13',
+    version: '4.1.14',
     ENV_NAME: 'CMB-PRO', // for different topic
 
     SDK: 'mbsdk.wellcloud.cc',
