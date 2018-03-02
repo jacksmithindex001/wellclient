45ff254424dee0d48fe2c31d051fe573206a4a93
wangduanduan <1779706607@qq.com>
2018-03-02 11:44:04 +0800

    修改aws https 日志路径的问题

--- public/js/well-client.js
+++ public/js/well-client.js
@@ -177,7 +177,7 @@ window.wellClient = (function ($) {
       protocol: 'https://',
       wsProtocol: 'wss://',
       autoAnswer: false,
-      logPrefix: 'api.wellcloud.cc/log'
+      logPrefix: 'api.wellcloud.cc'
     },
     'OUR-TEST': {
       SDK: '192.168.40.107',
