32b952aa7df88466886f1035a3c71a8e66b81f3b
wangduanduan <1779706607@qq.com>
2019-03-21 17:36:11 +0800

    version update

--- public/js/well-client.js
+++ public/js/well-client.js
@@ -13,7 +13,7 @@ window.wellClient = (function ($) {
   }
 
   var Config = {
-    version: '1.3.0',
+    version: '1.3.1',
     ENV_NAME: 'CMB-PRO', // for different topic
     sessionIdCookieName: 'wellclient-cookie-session-id',
 
